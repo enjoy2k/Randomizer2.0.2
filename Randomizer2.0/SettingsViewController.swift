486dd22f4880ad660d697d68aa22f050e2479193
enjoy2k <61755950+enjoy2k@users.noreply.github.com>
2023-08-22 12:11:12 +0300

    Initial Commit

---
+++ Randomizer2.0/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Randomizer2.0
+//
+//  Created by Евгения Максимова on 19.06.2023.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var minValueTF: UITextField!
+    @IBOutlet weak var maxValueTF: UITextField!
+    
+    var minimumValue: String!
+    var maximumValue: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        minValueTF.text = minimumValue
+        maxValueTF.text = maximumValue
+    }
+    
+    
+    @IBAction func cancelButtonPressed() {
+        dismiss(animated: true)
+    }
+    
+}
